dfa6a11a76fc5c3351662bad2028e3a5d76a4387
folushojimi <iamrblogger@gmail.com>
2024-09-14 07:29:05 +0100

    javascript array

---
+++ script.js
@@ -0,0 +1,67 @@
+const months =  ['January', 'Febuary', 'March', 'April']
+console.log(months[0])
+
+
+
+const myArray = new Array (
+    'kola',
+    'bose',
+    'bimbo'
+)
+
+console.log(myArray)
+
+
+const listOfstaff = new Array(
+
+    'james',
+     'john',
+     'kemi'
+)
+
+ console.log(listOfstaff)
+
+ const newStudent = [
+    'shade',
+    'pedro',
+    'pablo'
+ ]
+
+ console.log(newStudent)
+
+
+ const favouriteFruits = ['apple', 'banana', 'banana', 'orange']
+ // Array Methods 
+
+// push
+ favouriteFruits.push('mango', 'pineapple', 'coco nut')
+
+ //pop
+ favouriteFruits.pop()
+
+ //shift
+ favouriteFruits.shift()
+
+ //unshift
+ favouriteFruits.unshift('lemon', 'grape')
+
+ //indexOf
+ const indexTesting = favouriteFruits.indexOf('banana')
+
+
+//slice
+const topThreeFruits = favouriteFruits.slice(0,3)
+const lastFruit = favouriteFruits.slice(-1)
+
+console.log(favouriteFruits[0])
+console.log(favouriteFruits[1])
+console.log(favouriteFruits[2])
+console.log(favouriteFruits.length)
+console.log(favouriteFruits)
+console.log(indexTesting)
+console.log(topThreeFruits)
+console.log(lastFruit)
+
+
+
+ 
