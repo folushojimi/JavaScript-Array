fb2dc17238d6a7f314961575b11e95ad642d65a6
folushojimi <iamrblogger@gmail.com>
2024-09-14 16:04:26 +0100

    more array lectures

--- script.js
+++ script.js
@@ -3,65 +3,87 @@ console.log(months[0])
 
 
 
-const myArray = new Array (
-    'kola',
-    'bose',
-    'bimbo'
-)
-
-console.log(myArray)
 
 
-const listOfstaff = new Array(
-
-    'james',
-     'john',
-     'kemi'
-)
-
- console.log(listOfstaff)
-
- const newStudent = [
-    'shade',
-    'pedro',
-    'pablo'
- ]
-
- console.log(newStudent)
 
 
- const favouriteFruits = ['apple', 'banana', 'banana', 'orange']
- // Array Methods 
 
-// push
- favouriteFruits.push('mango', 'pineapple', 'coco nut')
+// //From The Book
+// const myArray = new Array (
+//     'kola',
+//     'bose',
+//     'bimbo'
+// )
 
- //pop
- favouriteFruits.pop()
-
- //shift
- favouriteFruits.shift()
-
- //unshift
- favouriteFruits.unshift('lemon', 'grape')
-
- //indexOf
- const indexTesting = favouriteFruits.indexOf('banana')
+// console.log(myArray)
 
 
-//slice
-const topThreeFruits = favouriteFruits.slice(0,3)
-const lastFruit = favouriteFruits.slice(-1)
+// const listOfstaff = new Array(
 
-console.log(favouriteFruits[0])
-console.log(favouriteFruits[1])
-console.log(favouriteFruits[2])
-console.log(favouriteFruits.length)
-console.log(favouriteFruits)
-console.log(indexTesting)
-console.log(topThreeFruits)
-console.log(lastFruit)
+//     'james',
+//      'john',
+//      'kemi'
+// )
 
+//  console.log(listOfstaff)
+
+//  const newStudent = [
+//     'shade',
+//     'pedro',
+//     'pablo'
+//  ]
+
+//  console.log(newStudent)
+
+
+//  const favouriteFruits = ['apple', 'banana', 'banana', 'orange']
+//  // Array Methods 
+
+// // push
+//  favouriteFruits.push('mango', 'pineapple', 'coco nut')
+
+//  //pop
+//  favouriteFruits.pop()
+
+//  //shift
+//  favouriteFruits.shift()
+
+//  //unshift
+//  favouriteFruits.unshift('lemon', 'grape')
+
+//  //indexOf
+//  const indexTesting = favouriteFruits.indexOf('banana')
+
+
+// //slice
+// const topThreeFruits = favouriteFruits.slice(0,3)
+// const lastFruit = favouriteFruits.slice(-1)
+
+// //splice
+// const testSplice = favouriteFruits.splice(0,2, 'cherry', 'avocado')
+
+// //join
+
+// // const testJOin = favouriteFruits.join(' and ')
+
+// // reverse()
+
+// const testReverse = favouriteFruits.reverse()
+
+// console.log(favouriteFruits[0])
+// console.log(favouriteFruits[1])
+// console.log(favouriteFruits[2])
+// console.log(favouriteFruits.length)
+// console.log(favouriteFruits)
+// console.log(indexTesting)
+// console.log(topThreeFruits)
+// console.log(lastFruit)
+// // console.log(testJOin)
+// console.log(testReverse)
 
 
  
+
+
+
+
